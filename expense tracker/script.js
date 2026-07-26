8f1077e511f1efed89a93ebb0238a26d4b46e93c
skardude12345 <souhardyakar11@gmail.com>
2026-07-26 12:24:44 +0530

    added local storage functionality to the expense tracker app to save and retrieve expenses, ensuring data persistence across page reloads.

--- expense tracker/script.js
+++ expense tracker/script.js
@@ -3,24 +3,30 @@ let amt = document.getElementById("amt")
 let addBtn = document.getElementById('add-expense')
 let listDisplay = document.getElementById('show-expenses')
 let updateBtn = document.getElementById('update-expense')
+
 updateBtn.style.display = 'none'
 
-let listArr = []
+let listArr = JSON.parse(localStorage.getItem('listArr')) || []
 
 let ind = -1
 
 function addExpense() {
+
+    if (!expenseName.value || !amt.value) return alert("Please fill out both fields")
+
     listArr.push({expenseName: expenseName.value, expenseAmt: amt.value})
-    expenseName.value = null
-    amt.value = null
+    localStorage.setItem('listArr', JSON.stringify(listArr))
+    
+    expenseName.value = ""
+    amt.value = ""
     showList()
 }
 
-
 function showList() {
-    listDisplay.innerHTML = null
-    for (let i = 0; i < listArr.length; i++) {
+    listDisplay.innerHTML = "" 
 
+
+    for (let i = 0; i < listArr.length; i++) {
         let div = document.createElement('div')
         let btnDiv = document.createElement('div')
 
@@ -32,16 +38,11 @@ function showList() {
 
         let editBtn = document.createElement('button')
         editBtn.innerText = 'edit'
-        editBtn.onclick = () => {
-            editExpense(i)
-        }
+        editBtn.onclick = () => editExpense(i)
 
         let delBtn = document.createElement('button')
         delBtn.innerText = 'delete'
-        delBtn.onclick = () => {
-            deleteExpense(i)
-        }
-
+        delBtn.onclick = () => deleteExpense(i)
 
         btnDiv.appendChild(editBtn)
         btnDiv.appendChild(delBtn)
@@ -50,12 +51,9 @@ function showList() {
         div.appendChild(amtH1)
         div.appendChild(btnDiv)
 
-
         div.classList.add('task-div')
-
         listDisplay.appendChild(div)
     }
-
 }
 
 function editExpense(index) {
@@ -70,17 +68,21 @@ function updateExpense() {
     listArr[ind].expenseName = expenseName.value
     listArr[ind].expenseAmt = amt.value
 
-    console.log(listArr)
+    localStorage.setItem('listArr', JSON.stringify(listArr))
 
     addBtn.style.display = 'inline'
     updateBtn.style.display = 'none'
 
-    expenseName.value = null
-    amt.value = null
+    expenseName.value = ""
+    amt.value = ""
     showList()
 }
 
 function deleteExpense(index) {
     listArr.splice(index, 1)
+    
+    localStorage.setItem('listArr', JSON.stringify(listArr))
     showList()
 }
+
+showList()
